4671d330ea86bcb2dbe8fa4d5f2e85a4d0bcde48
knemanti <kaveh.nemantipour@volvocars.com>
2023-07-18 14:25:30 +0200

    Hello from windows laptop

--- chapter07/main.cpp
+++ chapter07/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
 int main() {
-    std::cout << "We are testing github webhooks here!" << std::endl;
+    std::cout << "Hello from Windows laptop" << std::endl;
     return 0;
 }
