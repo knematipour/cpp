cb979b93996e4ba562e9ab7ce754df127694b32a
kaveh <kaveh.nematipour@outlook.com>
2023-09-15 22:45:35 +0200

    Basic constructors provided.

--- src/objectsAndClassesBasics.cpp
+++ src/objectsAndClassesBasics.cpp
@@ -7,6 +7,12 @@
 class SpreadsheetCell
 {
 public:
+    // changed for providing multiple constructors
+    // defauld constructor is provided ONLY when no constructor is defined!
+    // The problem called Most Vexing Parse
+    SpreadsheetCell() = default; // adds a compiler-generated default constructor to the class 
+    SpreadsheetCell(double initialValue);
+    SpreadsheetCell(std::string initialValue);
     void setValue(double value);
     double getValue() const;
 
@@ -18,6 +24,16 @@ private:
     double stringToDouble(std::string s) const;
     double m_value{0};
 };
+
+SpreadsheetCell::SpreadsheetCell(double d)
+{
+    setValue(d);
+}
+
+SpreadsheetCell::SpreadsheetCell(std::string s)
+{
+    setString(s);
+}
 
 void SpreadsheetCell::setValue(double value)
 {
@@ -54,16 +70,16 @@ double SpreadsheetCell::stringToDouble(std::string str) const
 void test_classes_basics()
 {
     // creating objects on the stack
-    SpreadsheetCell c1, c2;
+    SpreadsheetCell c1{0}, c2{0};
     c1.setValue(5);
     c2.setString("3.2");
     std::cout << "The value to c1 is set to: " << c1.getString() << "\n";
     std::cout << "The value to c2 is set to: " << c2.getString() << "\n";
 
     // creating objects on the heap
-    SpreadsheetCell *p1{new SpreadsheetCell{}};
-    std::unique_ptr<SpreadsheetCell> p2{std::make_unique<SpreadsheetCell>()};
-    auto p3{std::make_shared<SpreadsheetCell>()};
+    SpreadsheetCell *p1{new SpreadsheetCell{0}};
+    std::unique_ptr<SpreadsheetCell> p2{std::make_unique<SpreadsheetCell>(0)};
+    auto p3{std::make_shared<SpreadsheetCell>(0)};
 
     p1->setValue(10);
     p2->setString("3.14");
